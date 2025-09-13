5c65ba6fbe4b3d25015c9fd993dd9fd736c2c75b
LOGESH-D <kit26.csbs30@gmail.com>
2025-09-13 21:28:51 +0530

    stream, sentry added

---
+++ backend/src/config/stream.js
@@ -0,0 +1,34 @@
+import {StreamChat} from "stream-chat";
+import {ENV} from "../config/env.js";
+
+const streamClient = StreamChat.getInstance(ENV.STREAM_API_KEY, ENV.STREAM_API_SECRET);
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        await streamClient.upsertUser(userData);
+        console.log("Stream user upserted successfully:", userData.name);
+        return userData;
+    } catch (error) {
+        console.error("Error upserting Stream user:", error);
+    }
+};
+
+export const deletetStreamUser = async (userId) => {
+    try {
+        await streamClient.upsertUser(userId);
+        console.log("Stream user deleted successfully:", userId);
+        return userId;
+    } catch (error) {
+        console.error("Error deleting Stream user:", error);
+    }
+};
+
+export const generateStreamToken = (userId) => {
+  try {
+    const userIdString = userId.toString();
+    return streamClient.createToken(userIdString);
+  } catch (error) {
+    console.log("Error generating Stream token:", error);
+    return null;
+  }
+};
